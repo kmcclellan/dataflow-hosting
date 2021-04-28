dfed232522153a832ecf24e6db17c1077f85d268
Kyle McClellan <kylemcclellan3@gmail.com>
2021-04-28 17:26:52 -0400

    Dataflow service

---
+++ Microsoft.Extensions.Hosting.Dataflow/DataflowService.cs
@@ -0,0 +1,98 @@
+﻿namespace Microsoft.Extensions.Hosting.Dataflow
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using System.Threading.Tasks.Dataflow;
+    using Microsoft.Extensions.Hosting;
+
+    /// <summary>
+    /// Base class for implementing <see cref="IHostedService"/> using dataflow components.
+    /// </summary>
+    public abstract class DataflowService : IHostedService, IDisposable
+    {
+        private readonly CancellationTokenSource cancellationSource = new();
+        private IDataflowBlock? head;
+
+        /// <summary>
+        /// Gets the collection of dataflow blocks involved in processing.
+        /// </summary>
+        /// <remarks>
+        /// Added blocks have their faults propagated and are allowed to complete during shutdown.
+        /// </remarks>
+        protected ICollection<IDataflowBlock> Processors { get; } = new HashSet<IDataflowBlock>();
+
+        /// <summary>
+        /// Gets a token triggered when the service is shutting down ungracefully.
+        /// </summary>
+        /// <remarks>
+        /// Should be passed to <see cref="DataflowBlockOptions.CancellationToken"/>.
+        /// </remarks>
+        protected CancellationToken CancellationToken => this.cancellationSource.Token;
+
+        /// <inheritdoc/>
+        public virtual Task StartAsync(CancellationToken cancellationToken)
+        {
+            this.head = this.StartPipeline();
+            this.Processors.Add(this.head);
+
+            foreach (var block in this.Processors.Where(x => x != this.head))
+            {
+                // Propagate faults upstream.
+                block.Completion.ContinueWith(
+                    (task, obj) => ((IDataflowBlock)obj!).Fault(task.Exception!),
+                    this.head,
+                    CancellationToken.None,
+                    TaskContinuationOptions.OnlyOnFaulted,
+                    TaskScheduler.Default);
+            }
+
+            return Task.CompletedTask;
+        }
+
+        /// <inheritdoc/>
+        public virtual Task StopAsync(CancellationToken cancellationToken)
+        {
+            if (this.head == null)
+            {
+                throw new InvalidOperationException("Not started");
+            }
+
+            this.head.Complete();
+            cancellationToken.Register(obj => ((CancellationTokenSource)obj!).Cancel(), this.cancellationSource);
+
+            return Task.WhenAll(this.Processors.Select(x => x.Completion));
+        }
+
+        /// <summary>
+        /// Initializes dataflow components and begins processing.
+        /// </summary>
+        /// <remarks>
+        /// <para>One block should function as the pipeline "head," receiving and propagating shutdown via <see cref="IDataflowBlock.Complete"/> or <see cref="IDataflowBlock.Fault"/>.</para>
+        /// <para>Additional blocks should be added to <see cref="Processors"/>.</para>
+        /// </remarks>
+        /// <returns>The pipeline head.</returns>
+        protected abstract IDataflowBlock StartPipeline();
+
+        /// <summary>
+        /// Disposes and/or releases resources used by the service.
+        /// </summary>
+        /// <param name="disposing"><see langword="true"/> to release both managed and unmanaged resources; <see langword="false"/> to release only unmanaged resources.</param>
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                this.cancellationSource.Dispose();
+            }
+        }
+
+        /// <inheritdoc/>
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -10,3 +10,37 @@ An extension of `Microsoft.Extensions.Hosting` for use with `System.Threading.Ta
 Add the NuGet package to your project:
 
     $ dotnet add package dataflow-hosting
+
+## Usage
+
+Implement a custom `DataflowService`:
+
+```c#
+class MyService : DataflowService
+{
+    protected override IDataflowBlock StartPipeline()
+    {
+        // Initialize one or more connected processing blocks.
+        var processor = new ActionBlock<string>(name =>
+        {
+            Thread.Sleep(1000);
+            Console.WriteLine($"Hello, {name}.");
+        });
+
+        // Start processing data.
+        foreach (var name in new[] { "Jennifer", "Greg", "Sandy" })
+        {
+            processor.Post(name);
+        }
+
+        // Return the "head" block that will handle shutdown.
+        return processor;
+    }
+}
+```
+
+Register with host:
+
+```c#
+hostBuilder.ConfigureServices(x => x.AddHostedService<MyService>());
+```
